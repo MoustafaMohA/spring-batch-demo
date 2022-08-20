7c66ad428f36c494ad2a0a0a6f826ec25582cfb9
Mostafa <mostafa.m3390@gmail.com>
2022-08-20 12:19:53 +0200

    Adding first spring-batch job

---
+++ src/main/java/com/mg/demo/ProjectConfig.java
@@ -0,0 +1,9 @@
+package com.mg.demo;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class ProjectConfig {
+}
